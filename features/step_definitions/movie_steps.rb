e7cc579014acccec042f285f051a5a59eeb6892e
eugene <jony.ukraine.cs@hotmail.com>
2012-03-18 03:06:14 -0700

    hw3 - 1,2a,2b

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,11 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  assert false, "Unimplmemented"
+  #assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -21,8 +22,20 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
+#When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  
+  rating_list.split(%r{\s*,\s*}).each_with_index {
+      |name, index|
+        if(!index) #first element
+          When %Q{I #{uncheck}check "ratings[#{name}]"}
+        else
+          And %Q{I #{uncheck}check "ratings[#{name}]"}
+        end
+    }
 end
+
+
